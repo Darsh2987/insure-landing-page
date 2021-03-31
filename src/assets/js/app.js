d4411a992d26841ee7cce7165e890f07d4b4d48b
Darsh <darsh2987@gmail.com>
2021-03-31 15:31:46 +0100

    moile menu

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,4 +1,27 @@
 import "../scss/imports.scss";
 
 // Load Event - run script once window has loaded
-window.addEventListener("load", () => {});
+window.addEventListener("load", () => {
+  const nav = document.querySelector("#header-nav");
+
+  // Mobile Menu, Hamburger and Close Icon function
+  function mobileMenu() {
+    const hamburgerIcon = document.querySelector("#hamburger-icon");
+    const closeMenuIcon = document.querySelector("#close-menu-icon");
+
+    hamburgerIcon.addEventListener("click", () => {
+      nav.classList.add("header__nav-show");
+      closeMenuIcon.classList.add("header__close-menu--toggle");
+      hamburgerIcon.classList.add("header__hamburger-icon--toggle");
+    });
+
+    closeMenuIcon.addEventListener("click", () => {
+      nav.classList.remove("header__nav-show");
+
+      hamburgerIcon.classList.toggle("header__hamburger-icon--toggle");
+      closeMenuIcon.classList.toggle("header__close-menu--toggle");
+    });
+  }
+
+  mobileMenu();
+});
